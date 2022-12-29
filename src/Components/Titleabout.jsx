816dbf80ff1c1eac95cf51c44d06c9b7c3f2791d
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2022-12-29 18:10:14 +0100

    Vercel effect with responsive wrap

--- src/Components/Titleabout.jsx
+++ src/Components/Titleabout.jsx
@@ -4,14 +4,15 @@ import './Titleabout.css';
 
 export const Titleabout = () => {
   return (
+
+    
     <h1>
-        <span style="--start-color:#007CF0; --end-color:#00DFD8; --content: 'Front';">
-            Front
-        </span>
-        <span style="--start-color:#7928CA; --end-color:#FF0080; --content: 'End'; --animation:a2;">
+        <span className='b-front'>
+            Front</span>
+        <span className='b-end'>
             End
         </span>
-        <span style="--start-color:#FF4D4D; animation-name:a3; --end-color:#F9CB28; --content: 'Dev'; --animation: a3">
+        <span className='b-dev'>
             Dev
         </span>
     </h1>
